6cab21d56d82fdf063b48aac4eef6346978bd3c2
danish <mohammedanasdanish41@gmail.com>
2023-04-29 00:31:33 +0530

    unnecessary files removed, project report added

--- src/main/java/com/music/Configs/CustomizeAuthenticationSuccessHandler.java
+++
@@ -1,3 +0,0 @@
-// package com.music.Configs;
-
-// CustomizeAuthenticationSuccessHandler
--- src/main/java/com/music/Repository/userRepo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.music.Repository.userRepo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.music.models.User;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByEmail(String email);
-    User findByUsername(String username);
-}
